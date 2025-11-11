fdafa3db642336f7d31fa06393310ed375c4945e
Lauren <146199366+laurendouan@users.noreply.github.com>
2025-11-11 16:03:33 -0500

    Debut du projet, aucun contenu cree

---
+++ ProjetRushHour/src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.TilePane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			TilePane root = (TilePane)FXMLLoader.load(getClass().getResource("RushHour.fxml"));
+			Scene scene = new Scene(root, 800, 800);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Mon projet");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
